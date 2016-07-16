3652685c4d349575beffba9ffe0fc7a71801b69c
Nicholas De Nova <nanovad@gmail.com>
2016-07-15 22:03:07 -0500

    Implement find_last_proposal_index. Remove TODOs for is_filibuster, implement simple is_vote.

--- eunomia/legislation.py
+++ eunomia/legislation.py
@@ -11,9 +11,32 @@ class Legislation:
 		self.logger.info("Init complete.")
 	
 	def is_filibuster(self, message):
-		# TODO: Check for filibustering.
-		return False
+		return not self.is_vote(message)
 
 	def is_vote(self, message):
-		# TODO: Check for vote.
-		return False
+		# TODO: Proper vote detection/evaluation.
+		if message == ":D":
+			return True
+		else:
+			return False
+	
+	def find_last_proposal_index(self, backlog):
+		# Since EunomiaBot uses 0-max as oldest-newest, we assume that's the format backlog is in and traverse accordingly.
+		self.logger.debug("Backlog length {}".format(len(backlog)))
+
+		# range(1, 0, -1) steps down from 1 to 0. However, it is end-bounding exclusive, so use range(1, -1, -1) in order to get [1, 0].
+
+		# For some reason, range(0, -1, -1) returns an empty range object.
+		# This is a workaround for that.
+		brange = list(range(len(backlog) - 1, -1, -1))
+		print(brange)
+		if not brange:
+			self.logger.debug("brange is empty. This is likely to happen if there is only one item in the backlog. Setting to [0].")
+			brange = [0]
+		for i in brange:
+			message = backlog[i]
+			if self.is_filibuster(message):
+				self.logger.debug("\"{}\" is filibuster at index {}".format(message, i))
+				return i
+		
+		return -1
