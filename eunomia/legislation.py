b3b131b08793ada47ed590c14ab00cd509fbeaa9
Nicholas De Nova <nanovad@gmail.com>
2016-07-24 15:10:51 -0500

    Completely rework legislation.

--- eunomia/legislation.py
+++ eunomia/legislation.py
@@ -32,25 +32,43 @@ class Legislation:
 
 	def dereference_if_vote(self, message, backlog, backlog_orig, votecount=0):
 		self.logger.debug("Message \"{}\" dereferencing...".format(message))
+
 		packed = self.get_packed_vote_index(message)
 		if packed == None:
-			# It's a proposal.
-			self.logger.debug("It's a proposal.")
+			# It's a proposal
+			self.active_proposal = len(backlog) - 1
 			if votecount == 3:
-				self.legislate(message, backlog_orig[-25:])
-			return message
+				self.legislate(backlog[self.active_proposal], backlog[-25:])
+			return
 
 		(nick, back_x) = packed
-		if nick == None and back_x == 0:
-			self.logger.debug("It's a basic :D. Dereferencing by recursion.")
-			return self.dereference_if_vote(backlog[len(backlog) - 2], backlog[:-1], backlog_orig, votecount + 1)
 
+		if nick == None:
+			if back_x == 0:
+				# It's a basic :D
+				self.logger.debug("Basic :D.")
+				self.dereference_if_vote(backlog[-2], backlog[:-1], backlog_orig, votecount + 1)
+			else:
+				self.logger.debug(":D~expr/:D~N")
+				self.dereference_if_vote(backlog[-back_x - 2], backlog, backlog_orig, votecount + 1)
 		else:
-			self.logger.debug("It's a nick: :D or :D~expr. Dereferencing by recursion.")
+			self.logger.debug("nick: :D")
+
+			votecount += 1
+			
+			nick_msgs = 0
+			for i in range(len(backlog) - 1, -1, -1):
+				print("i=" + str(i))
+				if backlog[i].startswith("<{}>".format(nick)):
+					if nick_msgs == back_x:
+						self.active_proposal = i - 1
+					nick_msgs += 1
 			if votecount == 3:
-				self.legislate(message, backlog_orig[-25:])
-			return self.dereference_if_vote(backlog[len(backlog) - back_x - 1], backlog[:-1], backlog_orig, votecount + 1)
-	
+				self.legislate(backlog[self.active_proposal], backlog[-25:])
+
+		if self.active_proposal != None:
+			self.active_proposal += 1
+
 	def legislate(self, message, context):
 		self.logger.info("Legislation for proposal \"{}\" succeeded.".format(message))
 		f = open("pending_proposals.txt", "a")
@@ -59,3 +77,6 @@ class Legislation:
 			f.write("{}\n".format(line))
 		f.write("\n") # Write one last newline.
 		f.close()
+
+		self.active_proposal = None
+		self.active_votes = 0
