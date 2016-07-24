c7b6d41efaea81cbb578021cb657250106ec5536
Nicholas De Nova <nanovad@gmail.com>
2016-07-24 16:33:38 -0500

    Add docstrings.

--- eunomia/legislation.py
+++ eunomia/legislation.py
@@ -1,3 +1,12 @@
+"""
+.. module:: legislation
+	:platform: Unix
+	:synopsis: Handles all of eunomia's legislation/vote tracking features.
+
+.. moduleauthor:: Nicholas De Nova <nanovad@gmail.com>
+
+"""
+
 import logging
 from enum import Enum
 
@@ -6,6 +15,14 @@ from re import compile as regex
 vote_matcher = regex(r'<[^>]+> (?:(?P<nick>\S+)[:,] )?:D(?:(?P<carots>\^+)|~(?P<ints>\d+)|~(?P<expr>.+))?$')
 
 class Legislation:
+	""" Class that contains all legislation related functions. 
+
+		:param fhandler: Logging handler for output to a file.
+		:type fhandler: logging.FileHandler
+		:param shandler: Logging handler for output to a stream.
+		:type shandler: logging.StreamHandler
+	"""
+
 	def __init__(self, fhandler, shandler):
 		self.logger = logging.getLogger("Legislation")
 		self.logger.setLevel(logging.DEBUG)
@@ -18,6 +35,12 @@ class Legislation:
 		self.active_proposal = None
 
 	def get_packed_vote_index(self, message):
+		""" Parses the message, determines if it's a vote or not, and returns various information about the type of vote and its attributes.
+
+			:param message: Message to parse.
+			:type message: str
+			:returns: A tuple, containing nick (or None), and, if applicable, and a backreference index, if applicable. Alternatively, returns None if the message was not matched to a known form of vote.
+		"""
 		match = vote_matcher.match(message)
 		if match is None:
 			return None
@@ -33,6 +56,23 @@ class Legislation:
 		return (match['nick'], 0)
 
 	def dereference_if_vote(self, message, backlog, backlog_orig, votecount=0):
+		""" 
+			Parses message and determines if it is a vote or proposal.
+			If it is a vote, determines type of vote, and dereferences the proposal the vote refers to.
+			If it is not a vote, saves the index of the proposal in the backlog.
+
+			If 3 votes are counted, legislates the pending proposal, see :func:`eunomia.legislation.Legislation.legislate`
+
+			:param message: The message to parse.
+			:param backlog: A potentially modified backlog (this is used during recursion; when the backlog is sliced to avoid recounting votes.)
+			:param backlog_orig: An original copy of the backlog, used mostly for context inthe event of a successful legislation.
+			:param votecount: Number of votes already counted during recursion.
+			:type message: str
+			:type backlog: list
+			:type backlog_orig: list
+			:type votecount: int
+		"""
+
 		self.logger.debug("Message \"{}\" dereferencing...".format(message))
 
 		packed = self.get_packed_vote_index(message)
@@ -74,6 +114,16 @@ class Legislation:
 			self.active_proposal += 1
 
 	def legislate(self, message, context):
+		""" Legislates a given message and context, writes these to file ``pending_proposals.txt``
+
+			Does **not** check if there were sufficient votes - it is up to the caller to determine this **before** ``legislate`` is called.
+
+			:param message: The message that was legislated.
+			:param context: The 'context' (backlog, preferably including votes) around the legislated message.
+			:type message: str
+			:type context: list
+		"""
+
 		self.logger.info("Legislation for proposal \"{}\" succeeded.".format(message))
 		f = open("pending_proposals.txt", "a")
 		f.write("[{}]\n".format(message))
