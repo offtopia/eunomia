df434c933d5d144849d19e5bd9956034e75f2082
Nicholas De Nova <nanovad@gmail.com>
2016-07-29 15:25:24 -0500

    Implement channel logger.

---
+++ eunomia/eunomialog.py
@@ -0,0 +1,42 @@
+import datetime
+import os
+
+class ChannelLogger:
+	def __init__(self, channel_name):
+		self.channel_name = channel_name
+
+		self.init_date = self.get_current_date()
+		self.date_now = self.init_date
+
+	def get_current_date(self):
+		datetime_now = datetime.datetime.utcnow()
+		return datetime.date(datetime_now.year, datetime_now.month, datetime_now.day)
+
+	def update_current_date(self):
+		# TODO: This could probably just be 'self.date_now = self.get_current_date()'.
+		date_new = self.get_current_date()
+		if date_new == self.date_now:
+			# There's no need to change the date - we're already up to date.
+			return
+
+		self.date_now = date_new
+
+	def update_log_filename(self):
+		# Make sure we're writing to the correct log file.
+		self.update_current_date()
+		self.log_dir = os.path.dirname(os.path.dirname(__file__)) + "/logs/channel/{}".format(self.channel_name)
+
+		# Create the log dir (recursively) if it does not exist.
+		if not os.path.exists(self.log_dir):
+			os.makedirs(self.log_dir)
+
+		self.log_filename = "{}/{}.log".format(self.log_dir, self.date_now)
+
+	def append(self, log_message):
+		# Make sure that we're writing to the right logfile.
+		self.update_log_filename()
+
+		# We assume log_message is already preformatted.
+		# This is basically a stub to append to the file right now.
+		with open(self.log_filename, 'a') as logfile:
+			logfile.write(log_message)
